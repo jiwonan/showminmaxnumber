92c4c402879dcdc375f906c2cace3ebe5164e43e
jiwonan <s2018s31@e-mirim.hs.kr>
2020-05-12 12:31:33 +0900

    2020.05.12 C# 실습문제 07

--- showminmaxnumber/Program.cs
+++ showminmaxnumber/Program.cs
@@ -10,6 +10,23 @@ namespace showminmaxnumber
     {
         static void Main(string[] args)
         {
+            int[] array = new int[5];
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int min = array[0];
+            int max = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (min > array[i])
+                    min = array[i];
+                if (max < array[i])
+                    max = array[i];
+            }
+            Console.WriteLine("가장 큰 수: " + max);
+            Console.WriteLine("가장 작은 수: " + min);
         }
     }
 }
